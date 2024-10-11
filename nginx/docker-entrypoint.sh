a669438aab8e526bcacb6aac2c0f059377444639
matheuscdd <matheuscdd@hotmail.com>
2024-10-11 16:59:45 -0300

    feat: redirect stage

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${DOMAIN}' < "/etc/nginx/conf.d/default.conf.template" > /etc/nginx/nginx.conf
+envsubst '${DOMAIN},${STAGING_URL}' < "/etc/nginx/conf.d/default.conf.template" > /etc/nginx/nginx.conf
 
 exec "$@"
