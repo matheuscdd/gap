26ee38687a4187876757cc2e10502ba598090e16
Claus <103610806+matheuscdd@users.noreply.github.com>
2024-10-15 07:00:41 -0300

    fix: cron env (#3)

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('db:dump')->everyFifteenMinutes()->runInBackground();
-// ->environments(['stage', 'prod']);
+Schedule::command('db:dump')->everyFifteenMinutes()->runInBackground()->environments(['prod']);
