1b987bb4b99553997760a51701b14d79245e69bc
matheuscdd <matheuscdd@hotmail.com>
2025-02-26 07:52:19 -0300

    hotfix: dump

--- laravel/scheduler.sh
+++ laravel/scheduler.sh
@@ -8,4 +8,5 @@ cd "$path" || exit
 [ ! -d "$logsDir" ] && mkdir "$logsDir"
 
 source .env.cron
-PGPASSWORD="$PGPASSWORD" POSTGRES_USER="$POSTGRES_USER" FORWARD_DB_PORT="$FORWARD_DB_PORT" POSTGRES_DB="$POSTGRES_DB" php artisan schedule:run >> "$logsDir/cron.log"
+envs=$(cat .env.cron | tr '\n' ' ')
+eval "$envs php artisan schedule:run >> $logsDir/cron.log"
